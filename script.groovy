c41629ff0aca774b43d9a370718f4459c7db6123
Haruna Dauda <haruna.dauda123@gmail.com>
2025-12-17 14:24:24 +0100

    Create groovy script

---
+++ script.groovy
@@ -0,0 +1,13 @@
+def buildApp() {
+  echo 'buidling the application..."
+}
+
+def testApp() {
+  echo 'testing the application..."
+}
+
+def deployApp() {
+  echo 'deploying the application..."
+}
+
+return this
